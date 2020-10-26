be50232bcfbfb42e5c2fe736ad9c1faedda590d6
Anastasiia Yarmilko <anastasiia.yarm@gmail.com>
2020-10-26 02:10:04 +0200

    Сделала задания 1-4

---
+++ js/task-3.js
@@ -0,0 +1,60 @@
+ class Storage {
+    constructor(items) {
+        this.items = items;
+      }
+
+    getItems() {
+        return this.items;
+    }
+
+    addItem(item) {
+        return this.items.push(item);
+    }
+
+    removeItem(item) {
+        const itemIndex = this.items.indexOf(item);
+
+        return this.items.splice(itemIndex, 1);
+    }
+}
+
+console.log(typeof Storage);
+// 'function'
+
+const goods = [
+'Нанитоиды',
+'Пролонгер',
+'Железные жупи',
+'Антигравитатор'
+];
+
+const storage = new Storage(goods);
+
+console.log(storage.getItems());
+/* [
+'Нанитоиды',
+'Пролонгер',
+'Железные жупи',
+'Антигравитатор'
+] */
+
+storage.addItem('Дроид');
+console.log(storage.getItems());
+/* [
+'Нанитоиды',
+'Пролонгер',
+'Железные жупи',
+'Антигравитатор',
+'Дроид'
+] */
+
+storage.removeItem('Пролонгер');
+console.log(storage.getItems());
+/* [
+'Нанитоиды',
+'Железные жупи',
+'Антигравитатор',
+'Дроид'
+] */
+
+
